d04fc15176111622a8e113495aed84542637ab7a
Justin Swanson <geeksunny@gmail.com>
2013-12-07 22:57:15 -0600

    New navigation & app structure.

---
+++ src/com/radicalninja/mybootx/Bootscreen.java
@@ -0,0 +1,97 @@
+package com.radicalninja.mybootx;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.Log;
+
+public class Bootscreen extends Canvas {
+	private Bitmap originalState;
+	private Bitmap workingCopy;
+	private Paint painter;
+
+	public Bootscreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bootscreen(Bitmap bitmap) {
+		//super(bitmap);
+		// Backing up this bitmap for easy reverting.
+		originalState = bitmap.copy(bitmap.getConfig(), false);
+		// Creating a self-contained working copy Bitmap
+		workingCopy = bitmap.copy(bitmap.getConfig(), true);
+		this.setBitmap(workingCopy);
+		// Creating the painter and initializing the default settings.
+		painter = new Paint();
+		painter.setAntiAlias(true);
+		painter.setTextAlign(Paint.Align.CENTER);
+		painter.setHinting(Paint.HINTING_ON);
+		painter.setSubpixelText(true);
+	}
+	
+	public Bitmap getBitmap() {
+		return workingCopy;
+	}
+
+	public void resetBitmap() {
+		workingCopy = originalState.copy(originalState.getConfig(), true);
+		this.setBitmap(workingCopy);
+	}
+
+	public void setColor(int color) {
+		painter.setColor(color);
+	}
+
+	public void setTextSize(float size) {
+		painter.setTextSize(size);
+	}
+	
+	public void setTypeface(Typeface typeface) {
+		painter.setTypeface(typeface);
+	}
+	
+	/**
+	 * Personalize the workingCopy bitmap with the given message string.
+	 * 
+	 * @param message The given message string.
+	 */
+	public void doPersonalization(String message) {
+		float x = this.getWidth() / 2;
+		float y = this.getHeight() * 0.8f;
+		
+		this.drawText(message, x, y, painter);
+	}
+	
+	public boolean saveBitmap(Context context) {
+		// TODO: Implement this? https://github.com/kswlee/Android-BitmapEx
+		File prefixDir = (context.getExternalFilesDir(null) != null) ? context.getExternalFilesDir(null) : context.getFilesDir();
+		Log.i("Bootscreen", "File path: "+prefixDir.toString()+"/"+"DemoFile.png");
+		File file = new File(prefixDir, "DemoFile.png");
+		boolean success = false;
+		try {
+			OutputStream os = new FileOutputStream(file);
+			workingCopy.compress(Bitmap.CompressFormat.PNG, 100, os);
+			os.flush();
+			os.close();
+			success = true;
+			Log.i("Bootscreen", "PNG BITMAP SAVED TO STORAGE.");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			Log.i("Bootscreen", "ERROR! FileNotFoundException: "+e.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+			Log.i("Bootscreen", "ERROR! IOException: "+e.toString());
+		}
+		
+		return success;
+	}
+	
+}
--- src/com/radicalninja/mybootx/MainActivity.java
+++ src/com/radicalninja/mybootx/MainActivity.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -16,92 +18,98 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	final MainActivity parent = this;
+	
+	DrawerLayout drawerLayout;
+	FrameLayout leftDrawer;
+	ImageView previewView;
+	Bootscreen bootscreen;
+	
+	EditText inputMessage;
+	SeekBar inputFontSize;
+	TextView textFontSizeValue;
+	Spinner inputColor;
+	Spinner inputTypeface;
+	Button buttonPreview;
+	Button buttonSave;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		final Button buttonTest = (Button) findViewById(R.id.button_test);
-		final ImageView view = (ImageView) findViewById(R.id.imageView1);
-		final EditText inputText = (EditText) findViewById(R.id.editText1);
-		
-		// Get the AssetManager
-		final AssetManager manager = getAssets();
-		// Read a bitmap from Assets
-		try {
-			InputStream open = manager.open("white_test.png");
-			final Bitmap bitmap = BitmapFactory.decodeStream(open);
-			// Assign the bitmap to an ImageView in this Layout
-			view.setImageBitmap(bitmap);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		buttonTest.setOnClickListener(new OnClickListener() {
+		setContentView(R.layout.drawer_layout);
+		// Interface bindings
+		LayoutInflater factory = getLayoutInflater();
+		FrameLayout contentFrame = (FrameLayout) findViewById(R.id.content_frame);
+		View bootControls = factory.inflate(R.layout.boot_controls, null);
+		contentFrame.addView(bootControls);
+		leftDrawer = (FrameLayout) findViewById(R.id.left_drawer);
+		View bootPreview = factory.inflate(R.layout.boot_preview, null);
+		leftDrawer.addView(bootPreview);
+		// Spinner lists and other controls
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		previewView = (ImageView) findViewById(R.id.imagePreview);
+		inputMessage = (EditText) findViewById(R.id.inputMessage);
+		// - Font Size Selection
+		inputFontSize = (SeekBar) findViewById(R.id.inputFontSize);
+		textFontSizeValue = (TextView) findViewById(R.id.textFontSizeValue);
+		inputFontSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			
-			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(), "Starting Test!", Toast.LENGTH_SHORT).show();
-
-				try {
-					InputStream open = manager.open("white_test.png");
-					Bitmap target = BitmapFactory.decodeStream(open);
-					Bitmap mTarget = target.copy(target.getConfig(), true);
-					
-					Canvas scratch = new Canvas(mTarget);
-					Paint paint = new Paint();
-
-					paint.setColor(Color.RED);
-					paint.setTextSize(48f);	//12 pt?
-					paint.setTypeface(Typeface.SANS_SERIF);
-					paint.setAntiAlias(true);
-					paint.setTextAlign(Paint.Align.CENTER);
-
-					// Use math here to determine how big the text printing will be, and thus where the middle ground on where to print it is.
-					float x = mTarget.getWidth() / 2;
-					float y = mTarget.getHeight() * 0.8f;
-					
-					// Print the text to the photo, then display on the imageView.
-					scratch.drawText(inputText.getText().toString(), x, y, paint);
-					view.setImageBitmap(mTarget);
-					
-					// Write out to a file on external storage
-					Log.i("PNG_SAVE", "FilesDir: "+ getFilesDir());	//-/data/data/com.radicalninja.mybootx/files
-					Log.i("PNG_SAVE", "ExternalFilesDir: "+ getExternalFilesDir(null));
-					File file = new File(getFilesDir(), "DemoFile.png");
-					try {
-						OutputStream os = new FileOutputStream(file);
-						mTarget.compress(Bitmap.CompressFormat.PNG, 100, os);
-						os.flush();
-						os.close();
-						Log.i("PNG_SAVE", "PNG BITMAP SAVED TO SD CARD.");
-						Log.i("Hub", "height = "+ mTarget.getHeight() + ", width = " + mTarget.getWidth());
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-						Log.i("PNG_SAVE", "FileNotFoundException: "+ e.toString());
-					} catch (IOException e) {
-						e.printStackTrace();
-						Log.i("PNG_SAVE", "IOException: "+ e.toString());
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
 				
-				Toast.makeText(getApplicationContext(), "Test Over!", Toast.LENGTH_SHORT).show();
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				textFontSizeValue.setText(String.format("%ddp", (int) parent.getFontSize() ));
 			}
 		});
-
-}
+		// - Color Selection
+		inputColor = (Spinner) findViewById(R.id.inputColor);
+		ArrayAdapter<CharSequence> adapterColor = ArrayAdapter.createFromResource(this, R.array.inputColor, android.R.layout.simple_spinner_item);
+		adapterColor.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		inputColor.setAdapter(adapterColor);
+		// - Typeface Selection
+		inputTypeface = (Spinner) findViewById(R.id.inputTypeface);
+		ArrayAdapter<CharSequence> adapterTypeface = ArrayAdapter.createFromResource(this, R.array.inputTypeface, android.R.layout.simple_spinner_item);
+		adapterTypeface.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		inputTypeface.setAdapter(adapterTypeface);
+		// - Preview Button
+		buttonPreview = (Button) findViewById(R.id.buttonPreview);
+		buttonPreview.setOnClickListener(previewButtonClicked);
+		// - Save Button
+		buttonSave = (Button) findViewById(R.id.buttonSave);
+		buttonSave.setOnClickListener(saveButtonClicked);
+		// Start off with the default image. //TODO: THIS WILL PULL FROM DEVICE LATER.
+		loadImage();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,4 +118,123 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	/**
+	 * OnClickListener for the Preview button
+	 */
+	OnClickListener previewButtonClicked = new OnClickListener() {
+		public void onClick(View v) {
+			// Just for debug purposes, a Toast!
+			//Toast.makeText(getApplicationContext(), "Starting Test!", Toast.LENGTH_SHORT).show();
+
+			// Reset the bootscreen to its original state for a new preview.
+			bootscreen.resetBitmap();
+			// Update settings for the bootscreen.
+			bootscreen.setTextSize(getFontSize());
+			bootscreen.setColor(getColor());
+			bootscreen.setTypeface(getTypeface());
+			// Write to the bootscreen.
+			bootscreen.doPersonalization(inputMessage.getText().toString());
+			// Update the preview.
+			previewView.setImageBitmap(bootscreen.getBitmap());
+			// Open the preview pane.
+			drawerLayout.openDrawer(leftDrawer);
+		}
+	};
+	
+	/**
+	 * OnClickListener for the Save button
+	 */
+	OnClickListener saveButtonClicked = new OnClickListener() {
+		public void onClick(View v) {
+			bootscreen.saveBitmap(getApplicationContext());
+			drawerLayout.closeDrawers();
+			Toast.makeText(getApplicationContext(), "Bootscreen saved to SD card!", Toast.LENGTH_SHORT).show();
+			Log.i("MainActivity", "Saved bitmap!");
+		}
+	};
+	
+	/**
+	 * Get float value of current selected font size.
+	 */
+	private float getFontSize() {
+		int fontSize = inputFontSize.getProgress() + 36;
+		return (float) fontSize;
+	}
+	
+	/**
+	 * Get integer value for the current selected color
+	 */
+	private int getColor() {
+		Map<String, Integer> colorMap = new HashMap<String, Integer>();
+		colorMap.put("BLACK", Color.BLACK);
+		colorMap.put("BLUE", Color.BLUE);
+		colorMap.put("CYAN", Color.CYAN);
+		colorMap.put("DKGRAY", Color.DKGRAY);
+		colorMap.put("GRAY", Color.GRAY);
+		colorMap.put("GREEN", Color.GREEN);
+		colorMap.put("LTGRAY", Color.LTGRAY);
+		colorMap.put("MAGENTA", Color.MAGENTA);
+		colorMap.put("RED", Color.RED);
+		colorMap.put("WHITE", Color.WHITE);
+		colorMap.put("YELLOW", Color.YELLOW);
+		
+		return colorMap.get((String) inputColor.getAdapter().getItem(inputColor.getSelectedItemPosition()));
+	}
+	
+
+	/**
+	 * Get integer value for the current selected typeface
+	 */
+	private Typeface getTypeface() {
+		Map<String, Typeface> typefaceMap = new HashMap<String, Typeface>();
+		typefaceMap.put("DEFAULT", Typeface.DEFAULT);
+		typefaceMap.put("DEFAULT_BOLD", Typeface.DEFAULT_BOLD);
+		typefaceMap.put("MONOSPACE", Typeface.MONOSPACE);
+		typefaceMap.put("SANS_SERIF", Typeface.SANS_SERIF);
+		typefaceMap.put("SERIF", Typeface.SERIF);
+		
+		return typefaceMap.get((String) inputTypeface.getAdapter().getItem(inputTypeface.getSelectedItemPosition()));
+	}
+
+	/**
+	 * Loads the default[debug] boot image from the app's assets folder into memory / the preview pane.
+	 */
+	private void loadImage() {
+		// Get the AssetManager
+		final AssetManager manager = getAssets();
+		// Read a bitmap from Assets
+		try {
+			InputStream open = manager.open("white_test.png");
+			Bitmap bitmap = BitmapFactory.decodeStream(open);
+			// Assign the bitmap to an ImageView in this Layout
+			previewView.setImageBitmap(bitmap);
+			// Load the bitmap into the Bootscreen object
+			bootscreen = new Bootscreen(bitmap);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Loads the given {@link com.android.graphics.Bitmap} into memory / the preview pane.
+	 * 
+	 * @param	bitmap	The given Bitmap object to load.
+	 */
+	private void loadImage(Bitmap bitmap) {
+		previewView.setImageBitmap(bitmap);
+	}
+	
+	/**
+	 * Handling the back-button when the preview pane is open.
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (drawerLayout.isDrawerOpen(leftDrawer)) {
+				drawerLayout.closeDrawers();
+				return false;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
 }

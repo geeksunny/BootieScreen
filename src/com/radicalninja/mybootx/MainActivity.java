0333aa70f1f0fb6998f72c66667a7a2a977dce46
Justin Swanson <geeksunny@gmail.com>
2013-12-08 16:38:51 -0600

    Cleaning up un-used includes.

--- src/com/radicalninja/mybootx/MainActivity.java
+++ src/com/radicalninja/mybootx/MainActivity.java
@@ -1,11 +1,7 @@
 package com.radicalninja.mybootx;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,9 +10,7 @@ import android.app.Activity;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
